cc6bf8b0c897c6e00ad983ece005af377ca828d2
rlam22 <robert.lam22@gmail.com>
2023-06-21 12:11:02 +1000

    test bucket list data

--- app.js
+++ app.js
@@ -9,8 +9,19 @@ const mongoClient = new MongoClient(process.env.MONGO_DB_CONNECTION_STRING);
 let testCollection;
 
 mongoClient.connect().then(_ => {
-    const db = mongoClient.db("test");
-    testCollection = db.collection("test");
+    const db = mongoClient.db("bucket-list");
+    testCollection = db.collection("bucket-list-data");
+
+    testCollection.find().toArray()
+    .then(docs => {
+        if (docs.length < 1) {
+            testCollection.insertMany([
+                { name: "Machu Picchu"      , description: "5th-century Inca citadel located in the Eastern Cordillera of southern Peru on a 2,430-meter (7,970 ft) mountain ridge" , image: "https://en.wikipedia.org/wiki/Machu_Picchu#/media/File:Machu_Picchu,_Peru.jpg", activity: "Hike" },
+                { name: "Vinicunca"         , description: "The Rainbow Mountain or Vinicunca is a mountain near Cusco in Peru"                                                     , image: "https://en.wikipedia.org/wiki/Vinicunca#/media/File:Rainbow_Mountain_Peru.jpg", activity: "Hike" },
+                { name: "Salar de Uyuni"    , description: "Salar de Uyuni is the world's largest salt flat, or playa, at over 10,000 square kilometres (3,900 sq mi) in area."     , image: "https://en.wikipedia.org/wiki/Salar_de_Uyuni#/media/File:Salar_de_Uyuni,_Bolivia,_2016-02-04,_DD_16-18_HDR.JPG", activity: "Hike" }
+            ])
+        }
+    })
 }).catch(error => {
     console.log(error);
 })
