23e6f708e55fb416740d04c3b953a99b67ba3fda
rlam22 <robert.lam22@gmail.com>
2023-06-22 18:56:54 +1000

    about and folders

--- client/js/controllers/about.js
+++ client/js/controllers/about.js
@@ -5,7 +5,8 @@ function renderAbout() {
         <h3>What is this page?</h3>
         <p>Welcome to this website. This is a page where users are able to store their bucket list!</p>
         <h3>How does this page work?</h3>
-        <p>Guests are able to view bucket lists that are available to inspire their own buckets lists! Want to make your own? Guests can create a user and login to start making their own bucket lists </p>
+        <p>Guests are able to view bucket lists that are available to inspire their own buckets lists! Want to make your own? Guests can create a user and login to start making their own bucket lists</p>
+        <p>To register for an account or to login, click here // add link later // !</p>
         `;
   }
   
