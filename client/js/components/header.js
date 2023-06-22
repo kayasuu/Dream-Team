73f0580540a936307311e6e609245c4601d34862
galit.moss <galitmoss@me.com>
2023-06-22 21:11:19 +1000

    frontend -  bucklist form to add on page, fixed bugs & margin in css by James & Galit

--- client/js/components/header.js
+++ client/js/components/header.js
@@ -1,20 +1,18 @@
-function renderHeader()
-
-{setHeaderHtml()};
-console
+function renderHeader() {
+  setHeaderHtml();
+}
+console;
 
 function setHeaderHtml() {
-
-const header = document.getElementById("header-nav");
-header.innerHTML = 
-`
+  const header = document.getElementById("header-nav");
+  header.innerHTML = `
 <h1> Bucket List </h1>
 <ul id="navlist">
 <li onClIck="renderBucketList()">Bucket List</li>
 <li onClick="renderAbout()">About</li>
+<li onClick="addBucketListForm()">Add Bucket List Item</li>
 </ul>
 
-`
-console.log("Header is working!");
-
+`;
+  console.log("Header is working!");
 }
