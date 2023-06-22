a5287fa887bd73f87c0090481b80ef9319690741
rlam22 <robert.lam22@gmail.com>
2023-06-22 18:53:32 +1000

    folders and about-page

---
+++ client/js/controllers/about.js
@@ -0,0 +1,11 @@
+function renderAbout() {
+    const page = document.getElementById("page");
+    page.innerHTML = `
+        <h1>About</h1>
+        <h3>What is this page?</h3>
+        <p>Welcome to this website. This is a page where users are able to store their bucket list!</p>
+        <h3>How does this page work?</h3>
+        <p>Guests are able to view bucket lists that are available to inspire their own buckets lists! Want to make your own? Guests can create a user and login to start making their own bucket lists </p>
+        `;
+  }
+  
