61545fb095989cc9ae445c1b066870b9e9635662
Jamespreston1 <preston_104@hotmail.com>
2023-06-24 15:43:37 +1000

    testing for James' git issues

--- client/js/components/bucketlist.js
+++ client/js/components/bucketlist.js
@@ -28,28 +28,58 @@ function renderList(bucketList) {
   const image = document.createElement("img");
   image.src = bucketList.image;
 
- const deleteButton =  document.createElement("button");
- deleteButton.textContent = "Delete";
- deleteButton.addEventListener("click", () => {
-    axios.delete(`api/bucket/${bucketList._id}`).then((_)=> {
-        renderBucketList();
-    })
- })
-
- const editDiv = document.createElement('div');
- editDiv.id = `edit-challenge-${bucketList._id}`;
- const editButton = document.createElement('button');
- editButton.textContent = 'Edit';
- editButton.addEventListener('click', () => {
-    renderEditForm(bucketList);
- })
- editDiv.append(editButton);
-
-
- div.append(name, description, activity, image,deleteButton,editDiv);
+  const deleteButton =  document.createElement("button");
+  deleteButton.textContent = "Delete";
+  deleteButton.addEventListener("click", () => {
+     axios.delete(`api/bucket/${bucketList._id}`).then((_)=> {
+         renderBucketList();
+     })
+  })
+ 
+  const editDiv = document.createElement('div');
+  editDiv.id = `edit-challenge-${bucketList._id}`;
+  const editButton = document.createElement('button');
+  editButton.textContent = 'Edit';
+  editButton.addEventListener('click', () => {
+     renderEditForm(bucketList);
+  })
+  editDiv.append(editButton);
+ 
+  div.append(name, description, activity, image,deleteButton,editDiv);
   console.log("bucketlist is working!");
-  console.log("our git issues are fixed!");
 return div;
 }
 
+ function renderEditForm(bucketList){
+  const form = document.createElement("form")
+  form.innerHTML = `
+  <label for="name">Name</label>
+  <input type="text" name="name" value="${bucketList.name}">
+  <label for="description">Description</label>
+  <input type="text" name="description" value="${bucketList.description}">
+  <label for="activity">Activity</label>
+  <input type="text" name="activity" value="${bucketList.activity}">
+  <label for="image">Image</label>
+  <input type="text" name="image" value="${bucketList.image}">
+  <input type="submit">
+  `;
+form.addEventListener("submit", (event)=>{
+event.preventDefault()
+const formData = new FormData(form);
 
+const data = {
+  name: formData.get("name"),
+  description: formData.get("description"),
+  activity: formData.get("activity"),
+  image: formData.get("image"),
+};
+
+axios.put(`/api/bucket/${bucketList._id}`, data).then((_)=>{
+  renderBucketList()
+}).catch((error)=>{
+  console.log(error)
+})
+})
+const page = document.getElementById("page")
+page.replaceChildren(form)
+ }
