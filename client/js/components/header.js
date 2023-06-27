99c8a4f6acc4c68dfceb9fa1c8367c7dc6f774c4
galit.moss <galitmoss@me.com>
2023-06-27 20:53:32 +1000

    more styling and class changes, new about me

--- client/js/components/header.js
+++ client/js/components/header.js
@@ -9,8 +9,8 @@ function setHeaderHtml() {
 <div class="container">
 <nav class="navbar navbar-expand-lg border-bottom border-bottom-dark">
 <ul id="navlist" class="navbar-nav me-auto mb-2 mb-lg-0">
-<li class="nav-item navbar-text" onClIck="renderBucketList()">Bucket List |</li>
 <li class="nav-item navbar-text" onClick="renderAbout()">About |</li>
+<li class="nav-item navbar-text" onClIck="renderBucketList()">Bucket List |</li>
 <li class="nav-item navbar-text" onClick="addBucketListForm()">Add Bucket List Item |</li>
 <li class="nav-item navbar-text" onClick="chatGPTform()">ChatGPT</li>
 </ul>
@@ -22,6 +22,5 @@ function setHeaderHtml() {
 </nav>
 </div>
 `;
-  console.log("Header is working!");
 }
 
