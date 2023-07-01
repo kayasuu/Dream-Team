1c740c728a3d88f2dbcb99ffb9f71087c0f279ea
Jamespreston1 <preston_104@hotmail.com>
2023-07-01 16:08:48 +1000

    Adding in reflections placeholders, and updating homepage.js

--- client/js/components/homepage.js
+++ client/js/components/homepage.js
@@ -4,11 +4,11 @@ function renderHomePage(){
     <div class="centre">
     <h2>Welcome to Bucket List</h2>
     <p class="display-p">
-    Welcome to Bucket List, the ultimate destination for travel enthusiasts and adventure seekers! Curate your dream destinations and activities, and let us help you bring your bucket list to life. 
-    Add your coveted travel spots from around the globe and the thrilling activities you're eager to experience. Our platform makes it effortless to organise and prioritise your travel goals.
+    Welcome to Bucket List, a place to log, explore and build your bucket list and reflect back on your journeys. Let us help you bring your bucket list to life!
+    Our platform makes it effortless to map out, explore ideas and visualise your travel goals.
     <br/>    <br/>
-    Explore the world with ease using our AI assistant, ChatGPT. Generate personalised itineraries based on your destination, number of days, and preferred activities. Whether it's a weekend getaway or a month-long adventure, 
-    ChatGPT optimises your time and ensures every moment counts. Join our community of travel enthusiasts, share your bucket list destinations, and get inspired by others. Your dreams of adventure are just a click away. Start building your travel story with Bucket List today!
+    Explore the world with ease using our AI assistant, we can it BucketList.AI! Generate personalised itineraries based on your destination, number of days, preferred activities and budget. Whether it's a weekend getaway or a month-long adventure, 
+    BucketList.AI optimises your time and ensures every moment counts. Join our community of travel enthusiasts, share your bucket list destinations, and get inspired by others. Your dreams of adventure are just a click away. Start building your travel story with Bucket List today!
     </p>
     <div class="img-div d-flex align-items-center">
     <img class="mx-auto" src="https://i.ibb.co/d4Px2MB/bucket-list-AI-pic.png" alt="person sits on a big silver bucket slightly hunched over looking at a book, positioned on a cliff looking over at a lake and some mountains"></img>
